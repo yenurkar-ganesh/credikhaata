ef80ed478ae93c4b70f4de576071cdf92b257937
yenurkar-ganesh <ganeshyenurkar4@gmail.com>
2025-05-03 17:11:55 +0530

    testing server

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/new", (req, res) => {
+  res.json({ message: "Hello from the new route!" });
+});
+
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on http://localhost:${process.env.PORT}`)
 );
